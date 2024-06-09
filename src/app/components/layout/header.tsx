e70c95c17ed8c69508b70bb9dc4f7d9b96db8d3c
Ruslan <rustamovruslan1205@gmail.com>
2024-06-09 19:44:45 +0400

    initial

---
+++ src/app/components/layout/header.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import { Icons } from "@/app/components/icons";
+
+interface HeaderProps {}
+
+export default function Header() {
+  return (
+    <div>
+      <Icons.logo />
+    </div>
+  );
+}
