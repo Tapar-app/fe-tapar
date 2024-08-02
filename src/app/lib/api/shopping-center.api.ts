037edf669702a2bf391e6ff565d9662f5d0571d9
Ruslan <rustamovruslan1205@gmail.com>
2024-08-02 09:31:28 +0400

    apis

---
+++ src/app/lib/api/shopping-center.api.ts
@@ -0,0 +1,36 @@
+import api from "@/app/lib/api/index";
+import { ResponseType } from "@/app/lib/types";
+import exp from "node:constants";
+import getImportExportSpecifierInfo from "sucrase/dist/types/util/getImportExportSpecifierInfo";
+
+const shoppingCenterBaseUrl = "/shopping-center";
+
+export type ShoppingCenter = {
+  id: number;
+  name: string;
+  location: string;
+  mapLink: string;
+  isActive: boolean;
+};
+
+export interface GetAllShoppingCentersResponse extends ResponseType {
+  object: ShoppingCenter[];
+}
+export interface GetShoppingCenterByIdResponse extends ResponseType {
+  object: {
+    id: number;
+    name: string;
+  };
+}
+export const ShoppingCenterApi = {
+  async getAll() {
+    return await api.get<GetAllShoppingCentersResponse>(
+      `${shoppingCenterBaseUrl}/all`,
+    );
+  },
+  async getById(shoppingCenterId: string) {
+    return await api.get<GetShoppingCenterByIdResponse>(
+      `${shoppingCenterBaseUrl}/get/bazaar/sc/${shoppingCenterId}`,
+    );
+  },
+};
