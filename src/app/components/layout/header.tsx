ea76600994534171eaec7bd61464b15c35a36820
Nariman <Nariman@MacBook-Pro--Nariman.local>
2024-08-06 03:13:05 +0400

    bugs were fixed

--- src/app/components/layout/header.tsx
+++ src/app/components/layout/header.tsx
@@ -6,13 +6,13 @@ interface HeaderProps {}
 
 export default function Header() {
   return (
-    <div className="fixed top-0 pl-[50px] pr-[50px] pt-[26px] pb-[26px] h-[100px] w-full">
+    <header className="sticky top-0 pl-[50px] pr-[50px] pt-[26px] pb-[26px] h-[100px] w-full">
       <div className={"flex items-center gap-[30px]"}>
         <Icons.logo />
         <Link href={"/categories"} className={"bg-[#FAFAFA] py-3 px-5"}>
           Bütün kateqoriyalar
         </Link>
       </div>
-    </div>
+    </header>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import HomeClient from "./components/HomeClient";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center h-screen space-y-10">
+    <main className="flex flex-col items-center justify-center 2xl:mt-[145px] xl:mt-[140px] lg:mt-[400px] md:mt-[300px] mt-[400px] iphone-6-portrait:mt-[130px] iphone-6-landscape:mt-[10px] iphone-6-plus-portrait:mt-[20px] iphone-12-pro-portrait:mt-[200px] iphone-14-pro-portrait:mt-[250px] ipad-air-landscape:mt-[160px] ipad-pro-landscape:mt-[300px] space-y-10">
       <HomeClient />
     </main>
   );
