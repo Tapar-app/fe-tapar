d99225ff2909077f808bd84a4b48f74ae2caa58c
Nariman <Nariman@MacBook-Pro--Nariman.local>
2024-08-24 18:02:46 +0400

    bug were fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import HomeClient from "./components/HomeClient";
-import {Suspense} from "react";
+import { Suspense } from "react";
 
 export default function Home() {
-    return (
-        <main className="flex flex-col items-center justify-center h-[calc(100vh-200px)] space-y-10">
-            <Suspense>
-                <HomeClient/>
-            </Suspense>
-        </main>
-    );
+  return (
+    <main className="flex flex-col items-center justify-center h-[calc(100vh-200px)] space-y-10">
+      <Suspense>
+        <HomeClient />
+      </Suspense>
+    </main>
+  );
 }
