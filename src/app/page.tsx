7414e1547d4d0a75d9300643fc4c3def962b4386
Nariman <Nariman@MacBook-Pro--Nariman.local>
2024-08-24 17:02:03 +0400

    some bugs were fixed; favicon was changed; social medias were added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
-import Image from "next/image";
-import CloseSquare from "./components/CloseSquare";
-import SearchIcon from "./components/SearchIcon";
-import MainTabs from "./components/MainTabs";
-import MainInput from "./components/MainInput";
 import HomeClient from "./components/HomeClient";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center 2xl:mt-[145px] xl:mt-[140px] lg:mt-[400px] md:mt-[300px] mt-[400px] iphone-6-portrait:mt-[130px] iphone-6-landscape:mt-[10px] iphone-6-plus-portrait:mt-[20px] iphone-12-pro-portrait:mt-[200px] iphone-14-pro-portrait:mt-[250px] ipad-air-landscape:mt-[160px] ipad-pro-landscape:mt-[300px] space-y-10">
+    <main className="flex flex-col items-center justify-center h-[calc(100vh-200px)] space-y-10">
       <HomeClient />
     </main>
   );
