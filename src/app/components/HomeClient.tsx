68985a76e9f45aa65516f96d7d1cefa653e77d38
Nariman <Nariman@MacBook-Pro--Nariman.local>
2024-08-24 17:44:52 +0400

    bugs were fixed

--- src/app/components/HomeClient.tsx
+++ src/app/components/HomeClient.tsx
@@ -9,7 +9,7 @@ const HomeClient: React.FC = () => {
   const [activeTab, setActiveTab] = useState<number>(0);
 
   return (
-    <div className="flex flex-col items-center min-h-[91vh]">
+    <div className="flex flex-col items-center md:min-h-[91vh] min-h-[81vh]">
       <div className="flex flex-col items-center justify-center flex-1 space-y-10">
         <div>
           <h1 className="xl:text-[60px] lg:text-[60px] sm:text-[45px] text-[40px] font-semibold text-center">
