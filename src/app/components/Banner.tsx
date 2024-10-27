2bb2bfc3d0e994f160ec197226884e3b1f65ca00
Nariman <Nariman@MacBook-Pro--Nariman.local>
2024-10-27 20:51:28 +0400

    category card bugs were fixed

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 function Banner() {
   return (
-    <div className="absolute md:top-[250px] lg:top-[350px] xl:top-[130px] 2xl:top-[200px]">
+    <div className="absolute md:top-[250px] lg:top-[350px] xl:top-[130px] 2xl:top-[140px]">
       <div className="relative w-[330px] h-[224px] md:w-[600px] md:h-[117px] lg:w-[681px] lg:h-[117px]">
         <Image src="/Banner.svg" alt="Banner" fill className="object-contain" />
       </div>
